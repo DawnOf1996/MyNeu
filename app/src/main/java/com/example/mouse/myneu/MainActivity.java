43df3bcf78df497800a22c03158abccc62a01956
DawnOf1996 <lxj892033444@163.com>
2016-12-12 22:31:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mouse/myneu/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.mouse.myneu;
+
+import com.lyp.networkhelper.activity.BaseActivity;
+
+
+public class MainActivity extends BaseActivity {
+
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initEvents() {
+
+    }
+}
