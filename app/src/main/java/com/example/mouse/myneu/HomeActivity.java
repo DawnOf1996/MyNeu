b4849057b4f94e637f47b0231b9dbbadc13163d8
DawnOf1996 <lxj892033444@163.com>
2016-12-13 16:08:32 +0800

    2016/12/13

--- app/src/main/java/com/example/mouse/myneu/HomeActivity.java
+++ app/src/main/java/com/example/mouse/myneu/HomeActivity.java
@@ -1,13 +1,27 @@
 package com.example.mouse.myneu;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import com.lyp.networkhelper.activity.BaseActivity;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends BaseActivity {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+    protected int getLayoutId() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initEvents() {
+
     }
 }
