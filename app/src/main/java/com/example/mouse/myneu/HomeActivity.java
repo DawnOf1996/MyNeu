e780eb39042dc6e89414e0d1d206b62addffe209
DawnOf1996 <lxj892033444@163.com>
2016-12-15 20:45:48 +0800

    15,first fragment

--- app/src/main/java/com/example/mouse/myneu/HomeActivity.java
+++ app/src/main/java/com/example/mouse/myneu/HomeActivity.java
@@ -1,9 +1,35 @@
 package com.example.mouse.myneu;
 
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.example.mouse.myneu.Adapter.TabViewPagerAdapter;
+import com.example.mouse.myneu.View.HotFragment;
+import com.example.mouse.myneu.View.JobFragment;
+import com.example.mouse.myneu.View.TeamFragment;
 import com.lyp.networkhelper.activity.BaseActivity;
+import com.lyp.networkhelper.view.DefaultLoadingLayout;
+import com.lyp.networkhelper.view.NetworkHelperLayout;
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
+import java.io.File;
 
 public class HomeActivity extends BaseActivity {
+    private Toolbar homeToolbar;
+    private ViewPager homeViewPager;
+    private TabLayout homeTab;
+    private TabViewPagerAdapter tabViewPagerAdapter;
 
+    private DefaultLoadingLayout mLoadingLayout;
+
+    public static String IMAGE_CACHE_PATH = "MyImageCache";//图片缓存路径
 
     @Override
     protected int getLayoutId() {
@@ -17,11 +43,66 @@ public class HomeActivity extends BaseActivity {
 
     @Override
     protected void initViews() {
+        initImageLoader();//优先初始化
+        mLoadingLayout = (DefaultLoadingLayout) NetworkHelperLayout.createDefaultLayout(this, homeViewPager);
+
+        homeToolbar = findView(R.id.home_toolbar);
+        homeToolbar.setTitle(getResources().getString(R.string.app_name));
+        homeToolbar.inflateMenu(R.menu.menu_main);
+
+        homeViewPager = findView(R.id.home_view_pager);
+        homeTab = findView(R.id.home_tab);
+        homeTab.addTab(homeTab.newTab().setText("岗位课程"));
+        homeTab.addTab(homeTab.newTab().setText("热门课程"));
+        homeTab.addTab(homeTab.newTab().setText("团队介绍"));
+        tabViewPagerAdapter = new TabViewPagerAdapter(getSupportFragmentManager());
+        tabViewPagerAdapter.addFragments(new JobFragment(), "岗位课程");
+        tabViewPagerAdapter.addFragments(new HotFragment(), "热门课程");
+        tabViewPagerAdapter.addFragments(new TeamFragment(), "团队介绍");
+        homeViewPager.setOffscreenPageLimit(3);
+        homeViewPager.setAdapter(tabViewPagerAdapter);
+        homeTab.setupWithViewPager(homeViewPager);
 
     }
 
     @Override
     protected void initEvents() {
+        homeToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
 
+                    case R.id.menu_logout:
+                        finish();
+                        startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void initImageLoader() {
+        File cacheDir = com.nostra13.universalimageloader.utils.StorageUtils
+                .getOwnCacheDirectory(getApplicationContext(), IMAGE_CACHE_PATH);
+
+        DisplayImageOptions defaultOptions = new DisplayImageOptions
+                .Builder()
+                .cacheInMemory(true)
+                .cacheOnDisc(true)
+                .build();
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration
+                .Builder(this)
+                .defaultDisplayImageOptions(defaultOptions)
+                .memoryCache(new LruMemoryCache(12 * 1024 * 1024))
+                .memoryCacheSize(12 * 1024 * 1024)
+                .discCacheSize(32 * 1024 * 1024)
+                .discCacheFileCount(100)
+//                .discCache(new UnlimitedDiscCache(cacheDir))
+                .threadPriority(Thread.NORM_PRIORITY - 2)                .tasksProcessingOrder(QueueProcessingType.LIFO)
+                .build();
+
+        ImageLoader.getInstance().init(config);
     }
 }
--- app/src/main/java/com/example/mouse/myneu/view/JobFragment.java
+++ app/src/main/java/com/example/mouse/myneu/view/JobFragment.java
@@ -1,30 +1,131 @@
-package com.example.mouse.myneu.view;
+package com.example.mouse.myneu.View;
 
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 
+import com.example.mouse.myneu.Adapter.JobAdapter;
 import com.example.mouse.myneu.R;
+import com.example.mouse.myneu.Util.Constant;
+import com.example.mouse.myneu.Util.SPUtils;
+import com.example.mouse.myneu.bean.Position;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.Callback;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Response;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class JobFragment extends Fragment {
-
-
-    public JobFragment() {
-        // Required empty public constructor
-    }
-
+public class JobFragment extends BaseFragment {
+    private RecyclerView rcJob;
+    private JobAdapter mAdapter;
+    private List<Position> mData = new ArrayList<>();
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_job, container, false);
+    protected int getLayoutId() {
+        return R.layout.fragment_job;
     }
 
+    @Override
+    protected void initViews(View view) {
+        rcJob = findView(R.id.job_course);
+        rcJob.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+    @Override
+    protected void initData() {
+
+        Map<String, String> map = new HashMap();
+        map.put("Cookie", (String) SPUtils.get(getContext(), "jsessionid", ""));
+
+        OkHttpUtils
+                .post()
+                .url(Constant.URL_POSITION_LIST)
+                .headers(map)
+                .build()
+                .execute(new Callback() {
+                    @Override
+                    public Object parseNetworkResponse(Response response, int id) throws Exception {
+                        return response.body().string();
+                    }
+
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+
+                    }
+
+                    @Override
+                    public void onResponse(Object response, int id) {
+                        mData.clear();
+                        String json = (String) response;
+
+
+                        try {
+                            JSONObject object = new JSONObject(json);
+                            JSONArray indexPositionList = object.getJSONArray("indexPositionList");
+                            if (indexPositionList == null || indexPositionList.length()==0) {
+                                toast("请重新登录");
+                                return;
+                            }
+                            for (int i = 0; i < indexPositionList.length(); i++) {
+                                JSONObject positionObject = indexPositionList.getJSONObject(i);
+                                Position position = new Position();
+                                position.setVideoNums(positionObject.getInt("videoNums"));
+                                position.setStudy(positionObject.getInt("isStudy"));
+                                position.setCourseNums(positionObject.getInt("courseNums"));
+                                position.setPostName(positionObject.getString("postName"));
+                                position.setPostDesc(positionObject.getString("postDesc"));
+                                position.setPostUrl(positionObject.getString("postUrl"));
+                                position.setCourseHours(positionObject.getInt("courseHours"));
+                                position.setPersonNums(positionObject.getInt("personNums"));
+                                position.setStudyDays(positionObject.getString("studyDays"));
+                                position.setTestNums(positionObject.getInt("testNums"));
+                                position.setPostId(positionObject.getInt("postId"));
+                                mData.add(position);
+                            }
+                            mAdapter = new JobAdapter(getContext(), mData);
+                            Log.d("","获得岗位数据"+mData.size()+"条");
+                            rcJob.setAdapter(mAdapter);
+
+                            mAdapter.setOnItemClickListener(new JobAdapter.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(View view, int position) {
+                                    Intent intent = new Intent(getActivity(), JobAdapter.class);
+                                    intent.putExtra("bean_position", mData.get(position));
+                                    startActivity(intent);
+                                }
+
+                                @Override
+                                public void onItemLongClick(View view, int position) {}
+                            });
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                });
+
+
+    }
+
+    @Override
+    protected void initEvents(View view) {
+    }
 }
